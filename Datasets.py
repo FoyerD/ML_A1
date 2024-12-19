c5aa92733a037c3ff422528df556fa03a1a215e0
FoyerD <amitfoye@post.bgu.ac.il>
2024-12-19 19:24:05 +0200

    now mesures all 3

--- Datasets.py
+++ Datasets.py
@@ -24,58 +24,61 @@ from SoftDecisionTreeClassifier import SoftDecisionTreeClassifier
 from SoftDecisionTreeRegression import SoftDecisionTreeRegressor
 from WeightedDecisionTreeClassifier import WeightedSoftDecisionTreeClassifier
 
-n = 100
-a = 0.05
 param_grid_test = {"n_runs": [100], "alpha": [0.01]}
-param_grid = {"n_runs": [10, 50], "alpha": [0.00001,0.01, 0.05]}
-mega_param_grid = {'n_runs': [10, 50, 100], 'alpha': [0.001, 0.01, 0.05]}
+param_grid = {"n_runs": [10, 50], "alpha": [0.01, 0.05,0.1]}
+mega_param_grid = {'n_runs': [10, 50, 100], 'alpha': [0.01, 0.05,0.1]}
 
 # ---------- Classification ----------
 
-def test_model_UCI(dataset, transform_label_func, encode=False, reverse_transform_label_func=None):
+def test_model_UCI(dataset, transform_label_func, encode=False):
     X = dataset.data.features
     if encode:
         X = pd.get_dummies(X, drop_first=False)
     y = dataset.data.targets
-    test_model_X_y(X, y, transform_label_func, reverse_transform_label_func)
+    test_model_X_y(X, y, transform_label_func)
 
-def test_model_csv(df, transform_label_func, label_col, encode=False, reverse_transform_label_func=None):
+def test_model_csv(df, transform_label_func, label_col, encode=False):
     df = df.dropna() # Drop Rows containing Nan as a label
     X = df.drop(label_col, axis=1)
     if encode:
         X = pd.get_dummies(X, drop_first=False)
     y = df[label_col]
-    test_model_X_y(X, y, transform_label_func, reverse_transform_label_func)
+    test_model_X_y(X, y, transform_label_func)
 
-def test_model_X_y(X, y, transform_label_func, reverse_transform_label_func=None):
+def test_model_X_y(X, y, transform_label_func):
     y = transform_label_func(y.to_numpy())
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    #hard_model = DecisionTreeClassifier()
-    #soft_model = GridSearchCV(SoftDecisionTreeClassifier(), param_grid_test, cv=2)
-
     # Testing Adaptive tree
-    soft_model = GridSearchCV(WeightedSoftDecisionTreeClassifier(), param_grid_test, cv=2)
-    hard_model = GridSearchCV(SoftDecisionTreeClassifier(), param_grid_test, cv=2)
-    
-    # soft_model = SoftDecisionTreeClassifier(n_runs=n, alpha=a)
-    classes = np.unique(y)
+    hard_model = DecisionTreeClassifier()
+    weighted_gs = GridSearchCV(WeightedSoftDecisionTreeClassifier(), mega_param_grid, cv=10)
+    soft_gs = GridSearchCV(SoftDecisionTreeClassifier(), mega_param_grid, cv=10)
 
+    weighted_gs.fit(X_train.values, y_train)
+    weighted_model = WeightedSoftDecisionTreeClassifier(n_runs=weighted_gs.best_estimator_["n_runs"], alpha=weighted_gs.best_estimator_["alpha"])
+    soft_gs.fit(X_train.values, y_train)
+    soft_model = SoftDecisionTreeClassifier(n_runs=soft_gs.best_estimator_["n_runs"], alpha=soft_gs.best_estimator_["alpha"])
+    
+    classes = np.unique(y)
     print("Soft descision tree classifier:")
     y_pred_soft = train_and_eval(X_train, X_test, y_train, y_test, soft_model, classes)
     cross_val_clf(X_train, y_train, soft_model, 10)
-    print("parameters: " + str(soft_model.best_params_))
+    print("parameters: " + str(soft_gs.best_params_) + "\n")
+    print("Weighted descision tree classifier:")
+    y_pred_weight = train_and_eval(X_train, X_test, y_train, y_test, weighted_model, classes)
+    cross_val_clf(X_train, y_train, weighted_model, 10)
+    print("parameters: " + str(weighted_gs.best_params_) + "\n")
     print("Regular descision tree classifier:")
     y_pred_hard = train_and_eval(X_train, X_test, y_train, y_test, hard_model, classes)
     cross_val_clf(X_train, y_train, hard_model, 10)
 
     if(len(classes) <= 2):
-        plot_roc_curve(y_test, y_pred_soft, y_pred_hard)
+        plot_roc_curve(y_test, y_pred_soft, y_pred_hard, y_pred_weight)
     else:
-        pass
+        pass #Not working
         #plot_roc_multi_curve(y_test,y_pred_soft, y_pred_hard, reverse_transform_label_func, list(classes))
 
-    confusion_matrix_plot(y_test, y_pred_soft, y_pred_hard)
+    confusion_matrix_plot(y_test, y_pred_soft, y_pred_hard, y_pred_weight)
 
 
 def train_and_eval(X_train, X_test, y_train, y_test, model, classes):
@@ -122,49 +125,13 @@ def plot_pr_curve(X_test, y_test, soft_clf, hard_clf):
 
     plt.tight_layout()
     plt.show()
-    
-def plot_roc_multi_curve(y_test, y_pred_soft, y_pred_hard, reverse_transform, classes):
-    y_test = reverse_transform(y_test)
-    y_pred_soft = reverse_transform(y_pred_soft)
-    y_pred_hard = reverse_transform(y_pred_hard)
 
-    y_onehot_test = y_test.transform(y_test)
-    for class_of_interest in classes:
-        class_id = np.flatnonzero(y_test.classes_ == class_of_interest)[0]
-
-        display = RocCurveDisplay.from_predictions(
-            y_onehot_test[:, class_id],
-            y_pred_soft[:, class_id],
-            name=f"{class_of_interest} vs the rest",
-            color="darkorange",
-            plot_chance_level=True,
-            despine=True,
-        )   
-        _ = display.ax_.set(
-            xlabel="False Positive Rate",
-            ylabel="True Positive Rate",
-            title="Soft {class_of_interest}-vs-Rest ROC curves",
-        )
-
-        display = RocCurveDisplay.from_predictions(
-            y_onehot_test[:, class_id],
-            y_pred_soft[:, class_id],
-            name=f"{class_of_interest} vs the rest",
-            color="darkorange",
-            plot_chance_level=True,
-            despine=True,
-        )   
-        _ = display.ax_.set(
-            xlabel="False Positive Rate",
-            ylabel="True Positive Rate",
-            title=f"Regular {class_of_interest}-vs-Rest ROC curves",
-        )
-
-def plot_roc_curve(y_test, y_pred_soft, y_pred_hard):
+def plot_roc_curve(y_test, y_pred_soft, y_pred_hard, y_pred_weighted):
     fpr_soft, tpr_soft, _ = roc_curve(y_test, y_pred_soft)
     fpr_hard, tpr_hard, _ = roc_curve(y_test, y_pred_hard)
+    fpr_weighted, tpr_weighted, _ = roc_curve(y_test, y_pred_weighted)
 
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(12, 6))
 
     # Soft decision tree
     axes[0].plot(fpr_soft, tpr_soft)
@@ -178,6 +145,11 @@ def plot_roc_curve(y_test, y_pred_soft, y_pred_hard):
     axes[1].set_xlabel('False Positive Rate')
     axes[1].set_ylabel('True Positive Rate')
 
+    axes[2].plot(fpr_weighted, tpr_weighted)
+    axes[2].set_title("Weighted Decision Tree Classifier")
+    axes[2].set_xlabel('False Positive Rate')
+    axes[2].set_ylabel('True Positive Rate')
+
     plt.tight_layout()
     plt.show()
 
@@ -190,11 +162,12 @@ def cross_val_clf(X, y, clf, f):
     print("%f accuracy with a standard deviation of %f" % (scores.mean(), scores.std()))
 
 
-def confusion_matrix_plot(y_test, y_pred_soft, y_pred_hard):
+def confusion_matrix_plot(y_test, y_pred_soft, y_pred_hard, y_pred_weight):
     cm_soft = confusion_matrix(y_test, y_pred_soft)
     cm_hard = confusion_matrix(y_test, y_pred_hard)
+    cm_weight = confusion_matrix(y_test, y_pred_soft, y_pred_weight)
 
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(12, 6))
 
     disp1 = ConfusionMatrixDisplay(confusion_matrix=cm_soft)
     disp1.plot(ax=axes[0])
@@ -203,6 +176,10 @@ def confusion_matrix_plot(y_test, y_pred_soft, y_pred_hard):
     disp2 = ConfusionMatrixDisplay(confusion_matrix=cm_hard)
     disp2.plot(ax=axes[1])
     axes[1].set_title("Regular Decision Tree Classifier Confusion Matrix")
+
+    disp3 = ConfusionMatrixDisplay(confusion_matrix=cm_weight)
+    disp3.plot(ax=axes[2])
+    axes[1].set_title("Weighted Decision Tree Classifier Confusion Matrix")
 
     plt.tight_layout()
 
@@ -235,12 +212,11 @@ def test_model_csv_reg(df, label_col, encode=False):
 
 def test_model_X_y_reg(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    soft_model = SoftDecisionTreeRegressor()
-    #soft_model = GridSearchCV(SoftDecisionTreeRegressor(), mega_param_grid, cv=5)
+    soft_model = GridSearchCV(SoftDecisionTreeRegressor(), mega_param_grid, cv=10)
     hard_model = DecisionTreeRegressor()
     print("Soft descision tree regressor:")
     y_soft_pred = train_and_eval_reg(X_train, X_test, y_train, y_test, soft_model)
-#    print("parameters: " + str(soft_model.best_params_))
+    print("parameters: " + str(soft_model.best_params_))
     print("Regular descision tree regressor:")
     y_hard_pred = train_and_eval_reg(X_train, X_test, y_train, y_test, hard_model)
     plot_regression_graph(y_test, y_soft_pred, y_hard_pred)
@@ -285,15 +261,6 @@ def plot_regression_graph(y_test, y_soft_pred, y_hard_pred):
     plt.show()
 
 
-# ---------- Dataset 1: Water quality -----------
-
-#def test_model_water(dataset):
-#    print(" ----- Dataset 1 - Water quality -----")
-#    test_model_csv(dataset, transform_label_water, label_col="Potability", encode=True)
-#    print("")
-     
-#def transform_label_water(lst):
-#    return lst
 
 # ---------- Dataset 1: Graduation -----------
 
@@ -350,12 +317,29 @@ def transform_label_mountain_vs_beaches(lst):
     return lst
 
 
+# ---------- Dataset 6: Water quality -----------
+
+def test_model_water(dataset):
+    print(" ----- Dataset 6 - Water quality -----")
+    test_model_csv(dataset, transform_label_water, label_col="Potability", encode=True)
+    print("")
+     
+def transform_label_water(lst):
+    return lst
+
+
+# ---------- Dataset 7: Wine quality -----------
+def test_model_wine(dataset):
+    print(" ----- Dataset 6 - Water quality -----")
+    test_model_UCI(dataset, transform_label_wine, encode=True)
+    print("")
+     
+def transform_label_wine(lst):
+    return lst
+
+
+
 def classification_data():
-    
-    # Dataset 1
-    #water = pd.read_csv("water_potability.csv")
-    #test_model_water(water)
-    
     # Dataset 1
     predict_students_dropout_and_academic_success = fetch_ucirepo(id=697)
     test_model_graduation(predict_students_dropout_and_academic_success)
@@ -376,13 +360,21 @@ def classification_data():
     mountains_vs_beaches = pd.read_csv("mountains_vs_beaches_preferences.csv")
     test_model_mountain_vs_beaches(mountains_vs_beaches)
 
+    # Dataset 6
+    water = pd.read_csv("water_potability.csv")
+    test_model_water(water)
+    
+    # Dataset 7
+    wine_quality = fetch_ucirepo(id=186)
+    test_model_wine(wine_quality)
+
 
 def regression_data():
 
     # Dataset Reg 1
     print(" ----- Dataset 1 Reg - Wine quality -----")
-    wine_quality = fetch_ucirepo(id=186) 
-    test_model_UCI_reg(wine_quality)
+    wine_quality = fetch_ucirepo(id=186)
+    test_model_UCI_reg(wine_quality, encode=True)
     print("")
 
     # Dataset Reg 2
@@ -411,7 +403,7 @@ def regression_data():
 
 
 def main():
-    #regression_data()
+    regression_data()
     print("\n\n")
     classification_data()
 
--- downloads.py
+++ downloads.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("abdullah0a/world-happiness-data-2024-explore-life")
+path = kagglehub.dataset_download("ulrikthygepedersen/speed-dating")
 
 print("Path to dataset files:", path)
